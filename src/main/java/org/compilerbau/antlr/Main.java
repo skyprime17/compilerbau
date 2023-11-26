793ae8635f51ab6f45083b9f5f1513ddd57a6a9f
Nicolas Moeller <n.moeller@great-value-group.com>
2023-11-26 17:17:20 +0100

    -

--- src/main/java/org/compilerbau/antlr/Main.java
+++ src/main/java/org/compilerbau/antlr/Main.java
@@ -1,7 +1,30 @@
 package org.compilerbau.antlr;
 
-public class Main {
-  public static void main(String[] args) {
+import java.io.FileReader;
+import java.io.Reader;
+import java.io.StringReader;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+public class Main {
+  public static void main(String... args) throws Exception {
+    if (args.length > 0) {
+      compile(new FileReader(args[0]));
+    }
+
+  }
+
+  public static Integer compile(String skript) throws Exception {
+    return compile(new StringReader(skript));
+  }
+
+  public static Integer compile(Reader skript) throws Exception {
+    var lexer = new GrLexer(CharStreams.fromReader(skript));
+    var parser = new GrParser(new CommonTokenStream(lexer));
+
+    var antlrTree = parser.start();
+
+    return 0;
   }
 }
