4c8a354ce990b94ec8faeebb61a833ec7824db9b
Nicolas Moeller <n.moeller@great-value-group.com>
2024-03-01 18:40:25 +0100

    -

--- src/main/java/org/compilerbau/ast/Attributes.java
+++ src/main/java/org/compilerbau/ast/Attributes.java
@@ -4,7 +4,6 @@ public class Attributes {
   public Typ typ = new Typ.Unknown();
   public boolean nullable = false;
   public boolean needsBoxing = true;
-  public ArgConvention argConvention = ArgConvention.BORROWED;
 
   @Override
   public String toString() {
